8e3ee045a30d25cd3bb43e0bdf0c66a6d7606844
Aleksandar Tadic <129499013+Tadija93@users.noreply.github.com>
2024-11-08 15:33:17 +0100

    Delete FullstackApp/restful-web-services/restful-web-services directory

--- FullstackApp/restful-web-services/restful-web-services/src/main/java/com/rest/webservices/restful_web_services/jpa/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rest.webservices.restful_web_services.jpa;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.rest.webservices.restful_web_services.user.User;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-
-}
